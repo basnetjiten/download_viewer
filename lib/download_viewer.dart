ff8af29d045c94a4e4304bcc81407bc5cb38de4c
Jiten Basnet <jiten.basnet@ebpearls.com>
2025-02-12 13:48:53 +0545

    :star: feat: expose directory helper

--- lib/download_viewer.dart
+++ lib/download_viewer.dart
@@ -5,7 +5,7 @@
 library download_viewer;
 
 export 'src/core/services/download_viewer.dart';
-
+export 'src/core/services/device_directory_helper.dart';
 
 export 'src/core/widgets/progress_dialog_widget.dart';
 export 'src/core/widgets/pdf_view_widget.dart';
